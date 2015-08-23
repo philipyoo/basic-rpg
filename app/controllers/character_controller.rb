c58ceb8c15a891405ce3c9f0e7536e309f68f26d
[01;31m[K"name"[m[K: "Philip Yoo <[01;31m[K"email"[m[K>
2015-08-23 14:55:27 -0700

    Update character profile and selection pages - incomplete

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -7,5 +7,30 @@ get '/profile/:user_id/character/:id' do
 end
 
 #
-post '/profile/:user_id/character/:id' do
+post '/profile/:id/' do
+  #check stats (atk + armor)
+
+  if total_stats > x
+    redirect
+  else
+    auth_current_user.characters.create!(:name => params[:name], :hp => 80, :xp => 0, :level => 1, :atk => params[:atk], :armor => params[:armor])
+  end
+
+  erb :"profile/#{auth_current_user.id}"
+end
+
+
+
+post '/register' do
+  @user = User.new
+  @user.username = params[:username]
+  @user.password = params[:password]
+
+  if @user.save
+    auth_login(@user)
+    redirect "/profile/#{auth_current_user.id}"
+  else
+    @form_error = "Unable to register you."
+    erb :'users/register'
+  end
 end
