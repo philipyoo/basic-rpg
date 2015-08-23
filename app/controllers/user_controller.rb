1c4ec05fecc48aba25e85f48e8e8a035f6bc23d6
[01;31m[K"name"[m[K: "Philip Yoo <[01;31m[K"email"[m[K>
2015-08-22 20:15:27 -0700

    Add login route and erb file

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,3 +3,21 @@ get '/' do
   erb :index
 end
 
+get '/login' do
+  @login = ''
+  erb :'users/login'
+end
+
+post '/login' do
+  @login = params[:login]
+  user = User.find_by(:login => @login)
+
+  if (user && user.password == params[:password])
+    auth_login(user)
+    flash[:message] = 'You are logged in.'
+    redirect '/profile'
+  else
+    @form_error = 'Unable to log you in.'
+    erb :'users/login'
+  end
+end
