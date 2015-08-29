dd724390a68eefc6c148b35577143b67bb580de2
[01;31m[K"name"[m[K: "Philip Yoo <[01;31m[K"email"[m[K>
2015-08-29 00:42:48 -0700

    Update JavaScript file, incomplete. Fix form submittal jquery

--- public/js/application.js
+++ public/js/application.js
@@ -1,269 +1,124 @@
 $(document).ready(function() {
-  checker();
+  //Line 3 Fixes materialize css animations
+  $('select').material_select();
+
+  statAdjuster();
+
+  statSubmittal();
+
+
 });
 
 
 
 
-  // $('#delete').on("change", function(){
-  //   $("#deleteform").show();
-  // })
+function SetInitialStats() {
+  MAX = 15;
+  this.total = 15;
+  this.atk = 0;
+  this.def = 0;
+}
 
+var statAdjuster = function() {
+  var setupCharStats = new SetInitialStats();
 
-//   var add1 = document.getElementById("add1");
-//   var sub1 = document.getElementById("sub1");
-//   var add2 = document.getElementById("add2");
-//   var sub2 = document.getElementById("sub2");
-//
-// //////////
+  //wrap all calls in here depending on action
+  $('button').on('click', function() {
+    var buttonName = $(this).attr("name");
+    var buttonClass = $(this).attr("class");
 
-///////////
-  //
+    //Look to refactor this part:
+    if ( buttonClass === "add") {
 
-  function SetInitialStats() {
-    MAX = 15;
-    this.total = 15;
-    this.atk = 0;
-    this.def = 0;
-  }
-
-
-  //how to invoke?????
-  var checker = function() {
-    var workThis = new SetInitialStats();
-    //wrap all calls in here depending on action
-    $('button').on('click', function(e) {
-      var buttonName = $(this).attr("name");
-      var buttonClass = $(this).attr("class")
-
-      console.log(buttonName)
-
-      console.log(e)
-
-      //this represents button
-
-
-      if ( buttonClass === "add") {
-        console.log("i'm in the if add")
-
-        //class add and compare name atk vs def (buttonName)
-        if ( buttonName === "atk") {
-          workThis.adjAtk(buttonName, 1);
-        } else if ( buttonName === "def") {
-          workThis.adjDef(buttonName, 1);
-        }
-
-      } else if ( buttonClass === "sub") {
-        console.log("i'm in the else if sub")
-
-        if ( buttonName === "atk") {
-          workThis.adjAtk(buttonName, -1);
-        } else if ( buttonName === "def") {
-          workThis.adjDef(buttonName, -1);
-        }
+      if ( buttonName === "atk") {
+        setupCharStats.adjAtk(buttonName, 1);
+      } else if ( buttonName === "def") {
+        setupCharStats.adjDef(buttonName, 1);
       };
 
-    })
-  }
+    } else if ( buttonClass === "sub") {
 
+      if ( buttonName === "atk") {
+        setupCharStats.adjAtk(buttonName, -1);
+      } else if ( buttonName === "def") {
+        setupCharStats.adjDef(buttonName, -1);
+      };
 
-  //where direction = -1 or 1
-  SetInitialStats.prototype.checkTotal = function(direction) {
-    return (this.total - direction <= MAX && this.total - direction >= 0);
-  }
-
-  SetInitialStats.prototype.checkAtk = function(direction) {
-    return (this.atk + direction <= MAX && this.atk + direction >= 0);
-  }
-
-  SetInitialStats.prototype.checkDef = function(direction) {
-    return (this.def + direction <= MAX && this.def + direction >= 0);
-  }
-
-
-  SetInitialStats.prototype.adjAtk = function(buttonName, direction) {
-    // if both checks pass true
-    console.log("i'm in adjAtk");
-
-    if (this.checkTotal(direction) && this.checkAtk(direction)) {
-      // change div#display-atk to display change depending on direction
-      this.atk += parseInt(direction);
-      this.total -= parseInt(direction);
-
-      console.log("this is the atk:::::")
-      console.log(this.atk)
-
-      //put in adjusted number inside
-      $("#display-" + buttonName).html(this.atk);
-      $("div#total").html(this.total);
     };
-  }
+  })
+};
 
 
-  SetInitialStats.prototype.adjDef = function(buttonName, direction) {
-    console.log("i'm in adjDef");
-    // if both checks pass true
-    if (this.checkTotal(direction) && this.checkDef(direction)) {
-      // change div#display-atk to display change depending on direction
-      this.def += parseInt(direction);
-      this.total -= parseInt(direction);
+//where direction = -1 or 1
+SetInitialStats.prototype.checkTotal = function(direction) {
+  return (this.total - direction <= MAX && this.total - direction >= 0);
+}
 
-      console.log("this is the def:::::")
-      console.log(this.def)
+SetInitialStats.prototype.checkAtk = function(direction) {
+  return (this.atk + direction <= MAX && this.atk + direction >= 0);
+}
 
-      //put in adjusted number inside
-      $("#display-" + buttonName).html(this.def);
-      $("div#total").html(this.total);
-    }
-  }
+SetInitialStats.prototype.checkDef = function(direction) {
+  return (this.def + direction <= MAX && this.def + direction >= 0);
+}
 
 
-  ///////####  .on('click', 'button', function())
+SetInitialStats.prototype.adjAtk = function(buttonName, direction) {
+  if (this.checkTotal(direction) && this.checkAtk(direction)) {
+    this.atk += parseInt(direction);
+    this.total -= parseInt(direction);
+
+    $("#display-" + buttonName).html(this.atk);
+    $("div#total").html(this.total);
+  };
+}
 
 
-  // SetInitialStats.prototype.adjAtk = function() {
-  //
-  //   $('#add1').on('click', function(e){
-  //     if (this.total > 0 && this.atk < MAX) {
-  //       this.total--;
-  //       this.atk++;
-  //     };
-  //   });
-  //
-  //   $('#sub1').on('click', function(e){
-  //     if (this.total > 0 && this.atk < MAX) {
-  //       this.total++;
-  //       this.atk--;
-  //     };
-  //   });
-  //
-  //   $('#a').html(this.atk);
-  //   $('#total').html(this.total);
-  // };
+SetInitialStats.prototype.adjDef = function(buttonName, direction) {
+  if (this.checkTotal(direction) && this.checkDef(direction)) {
+    this.def += parseInt(direction);
+    this.total -= parseInt(direction);
 
-  // SetInitialStats.prototype.addTotals = function() {
-    //these should return booleans. Just test for conditionals
-
-    //add to Total if:
-      // total is greater than 0
-      // atk is less than MAX
-      // atk is
-      // def is less than MAX
-
-    // if (this.total < MAX && this.)
-  //
-  //   if (this.total < 15) {
-  //     return this.total++;
-  //   } else {
-  //     return this.total;
-  //   };
-  // }
-  //
-  // SetInitialStats.prototype.subTotals = function() {
-  //   if (this.total > 0) {
-  //     return this.total--;
-  //   } else {
-  //     return this.total;
-  //   }
-  // }
+    $("#display-" + buttonName).html(this.def);
+    $("div#total").html(this.total);
+  };
+}
 
 
-  //
-  // SetInitialStats.prototype.adjDef = function() {
-  //   $('#add2').on('click', function(e){
-  //     e.preventDefault();
-  //     if (this.total > 0 && this.def < MAX) {
-  //       this.total--;
-  //       this.def++;
-  //     };
-  //   });
-  //
-  //   $('#sub2').on('click', function(e){
-  //     e.preventDefault();
-  //     if (this.total > 0 && this.def < MAX) {
-  //       this.total++;
-  //       this.def--;
-  //     };
-  //   });
-  //
-  //   $('#b').html(this.def);
-  //   $('#total').html(this.total);
-  // }
-  //
-  //
-  // function ControlAllThisMess() {
-  //   //
-  // }
-  //
-  //
-  //
-  // $('input#uniq-btn').on('submit', function(){
-  //   $('#ainput').html(workThis.atk);
-  //   $('#binput').html(workThis.def);
-  // })
+// Now transition the div numbers and transfer to form on submittal
+// Maybe AJAX??
+
+//NAME HERE NOT XFERRING. FIX THIS
+var statSubmittal = function () {
+  // on click action::
+
+  $('#createCharForm').submit(function() {
+    var getName = $('input#getName').attr('value');
+    console.log(getName);
+    console.log("yolo")
+    console.log($('input#getName'))
+
+    $('input#nameInput').val(getName);
+
+    var getAtk = $('#display-atk').text();
+    $('input#atkInput').val(parseInt(getAtk));
+
+    var getDef = $('#display-def').text();
+    $('input#defInput').val(parseInt(getDef));
 
 
+  })
+};
+//
+    // var inputName = $('#createCharForm')
+                  // .attr("type", "hidden")
+                  // .attr("name", "name").val(getName)
 
-
-  // NEED TO FIX
-  // var btn = document.getElementById("uniq-btn");
-  //
-  // btn.addEventListener("click", function(){
-  //
-  // })
-
-
-  // var total = 15;
-  // var a = 0;
-  // var b = 0;
-  // var MAX = 15
-  // $('#a').html(a);
-  // $('#b').html(b);
-  // $('#total').html(total);
-  //
-  //
-  // document.getElementById('add1').addEventListener('click', function () {
-  //   if (total > 0 && a < MAX) {
-  //     total--;
-  //     a++;
-  //     document.getElementById('ainput').innerHTML = a;
-  //     document.getElementById('a').innerHTML = a;
-  //     document.getElementById('total').innerHTML = total;
-  //   }
-  // });
-  //
-  // document.getElementById('sub1').addEventListener('click', function () {
-  //   if (total < MAX && a > 0) {
-  //     total++;
-  //     a--;
-  //     document.getElementById('ainput').innerHTML = a;
-  //     document.getElementById('a').innerHTML = a;
-  //     document.getElementById('total').innerHTML = total;
-  //   }
-  // });
-  //
-  // document.getElementById('add2').addEventListener('click', function () {
-  //   if (total > 0 && b < MAX) {
-  //     total--;
-  //     b++;
-  //     document.getElementById('binput').innerHTML = b;
-  //     document.getElementById('b').innerHTML = b;
-  //     document.getElementById('total').innerHTML = total;
-  //   }
-  // });
-  //
-  // document.getElementById('sub2').addEventListener('click', function () {
-  //   if (total < MAX && b > 0) {
-  //     b--;
-  //     total++;
-  //     document.getElementById('binput').innerHTML = b;
-  //     document.getElementById('b').innerHTML = b;
-  //     document.getElementById('total').innerHTML = total;
-  //   }
-  // });
-  //
-  // btn.addEventListener('click', function(){
-  //   document.getElementById('ainput').value = document.getElementById('a').innerHTML;
-  //   document.getElementById('binput').value = document.getElementById('b').innerHTML;
-  //
-  // })
+    // $('#createCharForm').append($(inputName));
+// var input = $("<input>", { type: "hidden", name: "mydata", value: "bla" }); $('#form1').append($(input));
+//
+//
+// var input = $("<input>")
+//                .attr("type", "hidden")
+//                .attr("name", "mydata").val("bla");
+// $('#form1').append($(input));
