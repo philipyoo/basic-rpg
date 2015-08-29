c2e251b95c83ee39e91be59b28acee8d952c6656
[01;31m[K"name"[m[K: "Philip Yoo <[01;31m[K"email"[m[K>
2015-08-28 23:21:16 -0700

    Fix javascript OO button usage for character stat assignment, look to refactor if time available

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  checker();
+});
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 
-  $('#delete').on("change", function(){
-    $("#deleteform").show();
-  })
+
+
+  // $('#delete').on("change", function(){
+  //   $("#deleteform").show();
+  // })
 
 
 //   var add1 = document.getElementById("add1");
@@ -16,32 +16,214 @@ $(document).ready(function() {
 //   var sub2 = document.getElementById("sub2");
 //
 // //////////
-//   function Character(add1, sub1){
-//     this.atk = function(){
-//
-//     };
-//     this.armor = function(){
-//
-//     };
-//   }
 
-  // new Character()
 ///////////
+  //
+
+  function SetInitialStats() {
+    MAX = 15;
+    this.total = 15;
+    this.atk = 0;
+    this.def = 0;
+  }
+
+
+  //how to invoke?????
+  var checker = function() {
+    var workThis = new SetInitialStats();
+    //wrap all calls in here depending on action
+    $('button').on('click', function(e) {
+      var buttonName = $(this).attr("name");
+      var buttonClass = $(this).attr("class")
+
+      console.log(buttonName)
+
+      console.log(e)
+
+      //this represents button
+
+
+      if ( buttonClass === "add") {
+        console.log("i'm in the if add")
+
+        //class add and compare name atk vs def (buttonName)
+        if ( buttonName === "atk") {
+          workThis.adjAtk(buttonName, 1);
+        } else if ( buttonName === "def") {
+          workThis.adjDef(buttonName, 1);
+        }
+
+      } else if ( buttonClass === "sub") {
+        console.log("i'm in the else if sub")
+
+        if ( buttonName === "atk") {
+          workThis.adjAtk(buttonName, -1);
+        } else if ( buttonName === "def") {
+          workThis.adjDef(buttonName, -1);
+        }
+      };
+
+    })
+  }
+
+
+  //where direction = -1 or 1
+  SetInitialStats.prototype.checkTotal = function(direction) {
+    return (this.total - direction <= MAX && this.total - direction >= 0);
+  }
+
+  SetInitialStats.prototype.checkAtk = function(direction) {
+    return (this.atk + direction <= MAX && this.atk + direction >= 0);
+  }
+
+  SetInitialStats.prototype.checkDef = function(direction) {
+    return (this.def + direction <= MAX && this.def + direction >= 0);
+  }
+
+
+  SetInitialStats.prototype.adjAtk = function(buttonName, direction) {
+    // if both checks pass true
+    console.log("i'm in adjAtk");
+
+    if (this.checkTotal(direction) && this.checkAtk(direction)) {
+      // change div#display-atk to display change depending on direction
+      this.atk += parseInt(direction);
+      this.total -= parseInt(direction);
+
+      console.log("this is the atk:::::")
+      console.log(this.atk)
+
+      //put in adjusted number inside
+      $("#display-" + buttonName).html(this.atk);
+      $("div#total").html(this.total);
+    };
+  }
+
+
+  SetInitialStats.prototype.adjDef = function(buttonName, direction) {
+    console.log("i'm in adjDef");
+    // if both checks pass true
+    if (this.checkTotal(direction) && this.checkDef(direction)) {
+      // change div#display-atk to display change depending on direction
+      this.def += parseInt(direction);
+      this.total -= parseInt(direction);
+
+      console.log("this is the def:::::")
+      console.log(this.def)
+
+      //put in adjusted number inside
+      $("#display-" + buttonName).html(this.def);
+      $("div#total").html(this.total);
+    }
+  }
+
+
+  ///////####  .on('click', 'button', function())
+
+
+  // SetInitialStats.prototype.adjAtk = function() {
+  //
+  //   $('#add1').on('click', function(e){
+  //     if (this.total > 0 && this.atk < MAX) {
+  //       this.total--;
+  //       this.atk++;
+  //     };
+  //   });
+  //
+  //   $('#sub1').on('click', function(e){
+  //     if (this.total > 0 && this.atk < MAX) {
+  //       this.total++;
+  //       this.atk--;
+  //     };
+  //   });
+  //
+  //   $('#a').html(this.atk);
+  //   $('#total').html(this.total);
+  // };
+
+  // SetInitialStats.prototype.addTotals = function() {
+    //these should return booleans. Just test for conditionals
+
+    //add to Total if:
+      // total is greater than 0
+      // atk is less than MAX
+      // atk is
+      // def is less than MAX
+
+    // if (this.total < MAX && this.)
+  //
+  //   if (this.total < 15) {
+  //     return this.total++;
+  //   } else {
+  //     return this.total;
+  //   };
+  // }
+  //
+  // SetInitialStats.prototype.subTotals = function() {
+  //   if (this.total > 0) {
+  //     return this.total--;
+  //   } else {
+  //     return this.total;
+  //   }
+  // }
+
+
+  //
+  // SetInitialStats.prototype.adjDef = function() {
+  //   $('#add2').on('click', function(e){
+  //     e.preventDefault();
+  //     if (this.total > 0 && this.def < MAX) {
+  //       this.total--;
+  //       this.def++;
+  //     };
+  //   });
+  //
+  //   $('#sub2').on('click', function(e){
+  //     e.preventDefault();
+  //     if (this.total > 0 && this.def < MAX) {
+  //       this.total++;
+  //       this.def--;
+  //     };
+  //   });
+  //
+  //   $('#b').html(this.def);
+  //   $('#total').html(this.total);
+  // }
+  //
+  //
+  // function ControlAllThisMess() {
+  //   //
+  // }
+  //
+  //
+  //
+  // $('input#uniq-btn').on('submit', function(){
+  //   $('#ainput').html(workThis.atk);
+  //   $('#binput').html(workThis.def);
+  // })
+
+
 
 
   // NEED TO FIX
   // var btn = document.getElementById("uniq-btn");
+  //
+  // btn.addEventListener("click", function(){
+  //
+  // })
+
+
   // var total = 15;
   // var a = 0;
   // var b = 0;
-  // var max = 15
+  // var MAX = 15
   // $('#a').html(a);
   // $('#b').html(b);
   // $('#total').html(total);
   //
   //
   // document.getElementById('add1').addEventListener('click', function () {
-  //   if (total > 0 && a < max) {
+  //   if (total > 0 && a < MAX) {
   //     total--;
   //     a++;
   //     document.getElementById('ainput').innerHTML = a;
@@ -51,7 +233,7 @@ $(document).ready(function() {
   // });
   //
   // document.getElementById('sub1').addEventListener('click', function () {
-  //   if (total < max && a > 0) {
+  //   if (total < MAX && a > 0) {
   //     total++;
   //     a--;
   //     document.getElementById('ainput').innerHTML = a;
@@ -61,7 +243,7 @@ $(document).ready(function() {
   // });
   //
   // document.getElementById('add2').addEventListener('click', function () {
-  //   if (total > 0 && b < max) {
+  //   if (total > 0 && b < MAX) {
   //     total--;
   //     b++;
   //     document.getElementById('binput').innerHTML = b;
@@ -71,7 +253,7 @@ $(document).ready(function() {
   // });
   //
   // document.getElementById('sub2').addEventListener('click', function () {
-  //   if (total < max && b > 0) {
+  //   if (total < MAX && b > 0) {
   //     b--;
   //     total++;
   //     document.getElementById('binput').innerHTML = b;
@@ -85,6 +267,3 @@ $(document).ready(function() {
   //   document.getElementById('binput').value = document.getElementById('b').innerHTML;
   //
   // })
-
-
-});
